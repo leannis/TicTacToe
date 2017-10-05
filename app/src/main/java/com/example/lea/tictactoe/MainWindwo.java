f72ef0fa39dcbea1c4a17ada7e2e8d3bc759c3fb
Lea Kleesattel <32542772+leannis@users.noreply.github.com>
2017-10-05 14:56:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lea/tictactoe/MainWindwo.java
@@ -0,0 +1,13 @@
+package com.example.lea.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainWindwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_windwo);
+    }
+}
