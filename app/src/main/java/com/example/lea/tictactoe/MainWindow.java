9105e7450c46783754fbc6191833019350798a31
patrickginglseder <patrick.ginglseder@freenet.de>
2017-10-26 09:11:56 +0200

    sign up

--- app/src/main/java/com/example/lea/tictactoe/MainWindow.java
+++ app/src/main/java/com/example/lea/tictactoe/MainWindow.java
@@ -3,11 +3,14 @@ package com.example.lea.tictactoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainWindwo extends AppCompatActivity {
+public class MainWindow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_windwo);
+        setContentView(R.layout.activity_main_window);
     }
+
+
+
 }
