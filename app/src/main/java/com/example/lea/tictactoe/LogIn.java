f23a63f9a0309cfb3e4a3dc589e833777bf6faa8
patrickginglseder <patrick.ginglseder@freenet.de>
2017-11-30 11:48:41 +0100

    new Class Tools -Login Check -Register

--- app/src/main/java/com/example/lea/tictactoe/LogIn.java
+++ app/src/main/java/com/example/lea/tictactoe/LogIn.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
+import android.widget.Button;gi
 import android.widget.EditText;
 
 
